36c59249798cd6c6a8e5f5bd1549412393c86a56
Hanna Lee <mani21na@gmail.com>
2019-08-29 03:54:07 +0000

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,30 @@
 class Customer
+  attr_accessor :name, :age
+  attr_reader
+
+  @@all = []
+
+  def initialize(name, age)
+    @name = name
+    @age = age
+    @@all << self
+  end
   
+  # class method
+  def self.all
+    @@all
+  end
+  
+  # instance method
+  def new_meal(waiter, total, tip=0)
+    Meal.new(waiter, self, total, tip)
+  end
+
+  def meals
+    Meal.all.select{|meal| meal.customer == self}
+  end
+
+  def waiters
+    meals.map{|meal| meal.waiter}
+  end
 end
